44788bd84a0e046552f380feb87b19e443acb0fe
Solti Csongor Péter <solti.csongor.peter@gmail.com>
2024-09-15 01:07:02 +0200

    Update 1
    Full Rewrite/Recode
    
    :sob:

--- Solti-CsongorPeter-cipobolt.sql
+++ Solti-CsongorPeter-cipobolt.sql
@@ -4,11 +4,11 @@ SELECT * FROM `hirdetesek`;
 
 -- 4. feladat
 
-SELECT `gyarto`, `nev` FROM `hirdetesek`;
+SELECT `gyarto`, `nev` FROM `hirdetesek` WHERE nem = 'N';
 
 -- 5. feladat
 
-SELECT (`gyarto`, `nev`, `lejarat`) FROM `hirdetesek` WHERE `aktiv` = 1;
+SELECT `gyarto`, `nev`, DATE(`lejarat`) AS `lejarat` FROM `hirdetesek` WHERE aktiv = 0;
 
 -- 6. feladat
 
@@ -16,7 +16,7 @@ SELECT `gyarto`, `nev`, `meret` FROM `hirdetesek` WHERE `meret` <= 40 ORDER BY `
 
 -- 7. feladat
 
-SELECT * FROM `hirdetesek` WHERE `lejarat` >= TIME(YEAR(2024));
+SELECT * FROM `hirdetesek` WHERE `lejarat` <= '2024-12-31';
 
 -- 8. feladat
 
@@ -24,7 +24,7 @@ SELECT `gyarto`, `nev`, `meret` FROM `hirdetesek` WHERE `meret` >= 42;
 
 -- 9. feladat
 
-SELECT * FROM `hirdetesek` WHERE `gyarto` = Gabika and `gyarto` = Para;
+SELECT * FROM `hirdetesek` WHERE `gyarto` IN ('Gabika', 'Para');
 
 -- 10. feladat
 
@@ -32,36 +32,36 @@ SELECT `gyarto`, `ar` FROM `hirdetesek`;
 
 -- 11. feladat
 
-SELECT * FROM `hirdetesek`;
+SELECT `gyarto`, `ar` FROM `hirdetesek` WHERE `ar` BETWEEN 20000 AND 30000;
 
 -- 12. feladat
 
-SELECT * FROM `hirdetesek`;
+SELECT * FROM `hirdetesek` WHERE `gyarto` = 'Nipie' AND `szin` = 'fekete' AND MONTH(`lejarat`) = 1;
 
 -- 13. feladat
 
-SELECT * FROM `hirdetesek`;
+SELECT * FROM `hirdetesek` WHERE `nem` IN ('N', 'U');
 
 -- 14. feladat
 
-SELECT * FROM `hirdetesek`;
+SELECT * FROM `hirdetesek` ORDER BY `ar` ASC;
 
 -- 15. feladat
 
-SELECT * FROM `hirdetesek`;
+SELECT * FROM `hirdetesek` WHERE `aktiv` = 1 AND `lejarat` > NOW() ORDER BY `lejarat` ASC;
 
 -- 16. feladat
 
-SELECT * FROM `hirdetesek`;
+SELECT `gyarto`, `nev`, `ar` FROM `hirdetesek` ORDER BY `ar` DESC LIMIT 3;
 
 -- 17. feladat
 
-SELECT * FROM `hirdetesek`;
+SELECT * FROM `hirdetesek` WHERE `meret` > 40 ORDER BY `meret` ASC;
 
 -- 18. feladat
 
-SELECT * FROM `hirdetesek`;
+SELECT * FROM `hirdetesek` WHERE `lejarat` >= '2024-12-31' AND `aktiv` = 1;
 
 -- 19. feladat
 
-SELECT * FROM `hirdetesek`;
+SELECT * FROM `hirdetesek` WHERE `lejarat` = CURDATE();
